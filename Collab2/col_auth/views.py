fc76a11aaa293b6e28bb0f844c99ac691db9b906
Jing Lin <moderatelyfunctionalprogrammer@gmail.com>
2019-04-13 19:59:28 -0400

    basic base/index

--- Collab2/col_auth/views.py
+++ Collab2/col_auth/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	return HttpResponse('First!')
+	empty_context = dict()
+	return render(request, 'index.html', empty_context)
