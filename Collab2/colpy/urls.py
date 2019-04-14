3ada300f64635ffe4f4d85cfdf5553e46c6b39c7
Luke Ruter <lukea.ruter@gmail.com>
2019-04-14 10:44:52 -0400

    urls

--- Collab2/colpy/urls.py
+++ Collab2/colpy/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('create/', views.create),
     path('space/<custom_url>', views.space),
@@ -11,3 +14,4 @@ urlpatterns = [
     path('space/submit_python/', views.submit_python),
     path('space/fetch_submission/', views.fetch_submission),
 ]
+urlpatterns += static('space'+settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
